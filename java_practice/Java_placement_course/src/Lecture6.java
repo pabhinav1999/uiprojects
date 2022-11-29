a3b5d6b747e5b42c6e86126c7290c5bff3374415
Abhinav Kumar Pulijala <abhinavkumar.pulij@prod.hclpnp.com>
2022-11-29 08:00:58 +0530

    advanced java patterns apna college lecture 6

---
+++ java_practice/Java_placement_course/src/Lecture6.java
@@ -0,0 +1,86 @@
+class Lecture6 {
+    public static void main(String args[]){
+        int n=5;
+    //     for(int i=1;i<=n;i++){
+    //         int spaces = 2* (n-i);
+    //         for(int j=1;j<=i;j++)
+    //             System.out.print("*");
+    //         for(int j=1;j<=spaces;j++)
+    //            System.out.print(" ");
+    //         for(int j=1;j<=i;j++)
+    //            System.out.print("*");
+    //      System.out.println();
+    //     }
+
+    //    for(int i=n;i>=1;i--){
+    //     int spaces = 2* (n-i);
+    //     for(int j=1;j<=i;j++)
+    //         System.out.print("*");
+    //     for(int j=1;j<=spaces;j++)
+    //        System.out.print(" ");
+    //     for(int j=1;j<=i;j++)
+    //        System.out.print("*");
+    //  System.out.println();
+    //    }
+
+       System.out.println();
+       System.out.println();
+
+       for(int i=1;i<=n;i++){
+         for(int j=1;j<=n-i;j++){
+           System.out.print(" ");
+         }
+         for(int j=1;j<=n;j++){
+           System.out.print("*");
+         }
+         System.out.println();
+       }
+       
+       System.out.println();
+       System.out.println();
+
+       for(int i=1;i<=n;i++){
+         for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+         }
+         for(int j=1;j<=i;j++)
+          System.out.print(i+ " ");
+          System.out.println();
+       }
+      
+       System.out.println();
+       System.out.println();
+
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+           System.out.print(" ");
+        }
+        for(int j=i;j>=1;j--)
+         System.out.print(j);
+
+        for(int j=2;j<=i;j++)
+         System.out.print(j);
+         System.out.println();
+      }
+
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+         }
+         for(int j=1;j<=2*(i)-1;j++)
+          System.out.print("*");
+         System.out.println();
+      }
+       
+      for(int i=n;i>=1;i--){
+        for(int j=1;j<=n-i;j++){
+           System.out.print(" ");
+        }
+        for(int j=1;j<=2*(i)-1;j++)
+         System.out.print("*");
+         System.out.println();
+      }
+
+    }
+
+}
