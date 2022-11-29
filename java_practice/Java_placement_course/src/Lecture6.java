8a8214c43030d725d1be36ac8cdc9debd7365ded
Abhinav Kumar Pulijala <abhinavkumar.pulij@prod.hclpnp.com>
2022-11-29 08:02:25 +0530

    code formatting

--- java_practice/Java_placement_course/src/Lecture6.java
+++ java_practice/Java_placement_course/src/Lecture6.java
@@ -1,27 +1,27 @@
 class Lecture6 {
     public static void main(String args[]){
         int n=5;
-    //     for(int i=1;i<=n;i++){
-    //         int spaces = 2* (n-i);
-    //         for(int j=1;j<=i;j++)
-    //             System.out.print("*");
-    //         for(int j=1;j<=spaces;j++)
-    //            System.out.print(" ");
-    //         for(int j=1;j<=i;j++)
-    //            System.out.print("*");
-    //      System.out.println();
-    //     }
+        for(int i=1;i<=n;i++){
+            int spaces = 2* (n-i);
+            for(int j=1;j<=i;j++)
+                System.out.print("*");
+            for(int j=1;j<=spaces;j++)
+               System.out.print(" ");
+            for(int j=1;j<=i;j++)
+               System.out.print("*");
+         System.out.println();
+        }
 
-    //    for(int i=n;i>=1;i--){
-    //     int spaces = 2* (n-i);
-    //     for(int j=1;j<=i;j++)
-    //         System.out.print("*");
-    //     for(int j=1;j<=spaces;j++)
-    //        System.out.print(" ");
-    //     for(int j=1;j<=i;j++)
-    //        System.out.print("*");
-    //  System.out.println();
-    //    }
+       for(int i=n;i>=1;i--){
+        int spaces = 2* (n-i);
+        for(int j=1;j<=i;j++)
+            System.out.print("*");
+        for(int j=1;j<=spaces;j++)
+           System.out.print(" ");
+        for(int j=1;j<=i;j++)
+           System.out.print("*");
+     System.out.println();
+       }
 
        System.out.println();
        System.out.println();
