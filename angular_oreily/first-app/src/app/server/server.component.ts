199fb29a04e5ac8cc713573098ec8b4f490629b5
Abhinav Kumar Pulijala <abhinavkumar.pulij@prod.hclpnp.com>
2023-01-18 12:10:52 +0530

    comitting angular code of 2nd module of udemy

---
+++ angular_oreily/first-app/src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+ selector:'app-server',
+ template: `<h2>I am the first server</h2>`,
+ styles:[`h2 {
+    color:#786543;
+ }`
+
+ ]
+})
+export class ServerComponent {
+  
+}
