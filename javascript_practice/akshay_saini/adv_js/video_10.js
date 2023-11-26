14e11ff396671935c027a9d5d6966285bbbe230b
Abhinav <pabhinav1999@gmail.com>
2023-11-26 06:19:41 +0530

    Javascript modification for akshay saini course

---
+++ javascript_practice/akshay_saini/adv_js/video_10.js
@@ -0,0 +1,44 @@
+// {
+//     var a = 10;
+//     let b = 20;
+//     const c = 30;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// //var and var shadowing
+
+// var m = 100;
+// {
+//     var m = 10;
+//     let n = 20;
+// }
+// console.log(m);
+
+// let p = 100;
+// {
+//     let p = 20;
+// }
+// console.log(p);
+
+// const q = 75;
+// function z(){
+//     const q = 90;
+//     console.log(q);
+// }
+// console.log(q);
+
+// let j = 100 {
+//     var j = 10 ; //illegal shadowing
+// }
+
+// var k = 100;
+// {
+//     let k = 10;
+
+// }
---
+++ javascript_practice/akshay_saini/adv_js/video_2.js
@@ -0,0 +1,11 @@
+var n = 2;
+function square(n) {
+    let ans = n * n;
+    return ans;
+}
+
+var square2 = square(n);
+var square4 = square(4);
+
+console.log(square2);
+console.log(square4);
