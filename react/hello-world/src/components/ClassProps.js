2c6ced570910e69c5bea51168679dde98cdd9d9e
Abhinav <pabhinav1999@gmail.com>
2023-12-08 08:00:05 +0530

    React Commit

--- react/hello-world/src/components/ClassProps.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class ClassProps extends Component {
-  render(){
-    return <h1> Today is {this.props.day} of {this.props.month}</h1>
-  }
-}
-
-export default ClassProps;
