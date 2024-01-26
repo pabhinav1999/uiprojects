0596d995eb5bc4b63cbba4f49f64e8949d589faa
Abhinav <pabhinav1999@gmail.com>
2024-01-26 09:41:32 +0530

    Added git ignore

--- angular_projects/social_wall_project/social_wall/src/app/pages/login/login.component.ts
+++ angular_projects/social_wall_project/social_wall/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm : any;
+
+  constructor( private fb : FormBuilder) { }
 
   ngOnInit(): void {
+    this.initailiseForm();
   }
+
+  initailiseForm(){
+  this.loginForm = this.fb.group ( {
+    email : ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+}
+
+
+
 
 }
